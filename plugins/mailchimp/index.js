5b366f645f39639141a77a888f7cc089c15be1aa
Juho Häkkinen <juho@juhohakkinen.fi>
2015-10-15 16:48:25 +0300

    Added MailChimp plugin to tell where someone subcribes mailinglist

---
+++ plugins/mailchimp/index.js
@@ -0,0 +1,17 @@
+var config = require("./config.json");
+var mailchimp = require("mailchimp");
+
+module.exports = function(bot) {
+	var webhook = new MailChimpWebhook({
+		port: config.port,
+		secret: config.secret
+	});
+
+	webhook.on("subscribe", function (data, meta) {
+		bot.say(config.channel, "New subscriber joined to the mailing list!");
+	});
+
+	webhook.on("error", function(error) {
+		console.log(error.message);
+	});
+}
