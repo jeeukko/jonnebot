09003c6419e893d83e9f40e7276d83c287feb53b
Juho Häkkinen <juho@juhohakkinen.fi>
2016-01-26 18:16:28 +0200

    Add RSS-parser

---
+++ plugins/rss/index.js
@@ -0,0 +1,31 @@
+var config = require("./config.json");
+var fs = require("fs");
+var parser = require("rss-parser");
+
+module.exports = function(bot) {
+	setTimeout(function() {
+		//console.log("timeout");
+		setInterval(function() {
+			//console.log("interval");
+			parser.parseURL(config.url, function(err, parsed) {
+				//console.log("parser", err, parsed);
+				if (err) {
+					console.log("ERROR: " + err);
+				} else {
+					parsed.feed.entries.forEach(function(entry) {
+						if (new Date(entry.pubDate).getTime() / 1000 > config.lastPostTime) {
+							bot.say(bot.config.channel, "New message: " + entry.title + " | " + entry.link);
+
+							config.lastPostTime = new Date(entry.pubDate).getTime() / 1000;
+							fs.writeFile("./plugins/rss/config.json", JSON.stringify(config, null, 4), function(err) {
+								console.log("ERROR: " + err);
+							});
+						}
+					});
+				}
+			});
+		}, config.interval * 1000);
+	}, 10000);
+
+	//bot.say(bot.config.channel, config.messages[get.type]);
+}
